9106a21253730ba75d160fddd4d03a36455bc6f4
Parzaval35 <killian.prat35@gmail.com>
2025-05-27 09:22:52 +0200

    Update init_bdd.sql

--- init_bdd.sql
+++ init_bdd.sql
@@ -7,5 +7,6 @@ CREATE TABLE IF NOT EXISTS utilisateurs (
     pseudo VARCHAR(100) NOT NULL UNIQUE,
     email VARCHAR(150) NOT NULL UNIQUE,
     mot_de_passe VARCHAR(255) NOT NULL,
+    
     role ENUM('utilisateur', 'admin') DEFAULT 'utilisateur',
 );
