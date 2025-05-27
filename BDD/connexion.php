08c5814acbe670e5d249862c7512eeadd612165d
Samadi0 <chaymae.samadi16@gmail.com>
2025-05-27 09:53:15 +0200

    Update connexion.php

--- BDD/connexion.php
+++ BDD/connexion.php
@@ -23,7 +23,46 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         header("Location: index.php");
         exit;
     } else {
-        echo "Identifiants incorrects.";
+        $MessageErreur = "<p style='color:red;'>Identifiants incorrects</p>";
     }
 }
 ?>
+<!DOCTYPE HTML>
+<head>
+<meta charset="UTF-8">
+	<title>Connexion</title>
+	<style>
+        	body { 
+  			margin: 0;     /* Enlève les marges par défaut */
+  			height: 100vh; /* Prend 100% de la hauteur de la fenêtre */
+  			background-size: cover;    /* L’image couvre toute la zone */
+  			background-position: center; /* Centrée */
+  			background-repeat: no-repeat;
+			background-image: url("./images/fond_connexion.jpg");
+			color: black;
+			justify-content: center;   /* centrage horizontal */
+      			align-items: center;       /* centrage vertical */
+			text-align: center;
+			display: flex;
+		}
+		.box {
+      			background: #ccc;
+      			width: 300px;
+      			margin: 100px; 
+      			padding: 20px;
+      			border-radius: 5px;
+    		}
+	</style>
+</head>
+<div class= box>
+    <form method="POST" action="connexion.php">
+        Nom d’utilisateur : <input type="text" name="pseudo" required><br>
+	<br>
+        Mot de passe : <input type="password" name="mot_de_passe" required><br>
+	<br>
+        <input type="submit" value="Se connecter">
+	<?= $MessageErreur ?>
+    </form>
+</div>
+</body>
+</html>
