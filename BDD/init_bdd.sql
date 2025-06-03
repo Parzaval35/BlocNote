db2739e7c2ff33c227619d3e8c975efc57a02ca1
Samadi0 <chaymae.samadi16@gmail.com>
2025-06-03 10:32:10 +0200

    Update init_bdd.sql

--- BDD/init_bdd.sql
+++ BDD/init_bdd.sql
@@ -10,3 +10,23 @@ CREATE TABLE IF NOT EXISTS utilisateurs (
     
     role ENUM('utilisateur', 'admin') DEFAULT 'utilisateur',
 );
+
+CREATE TABLE `commentaires` (
+  `id_commentaire` int(11) NOT NULL,
+  `id_utilisateur` int(11) NOT NULL,
+  `contenu` text NOT NULL,
+  `id_parent` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+ALTER TABLE `commentaires`
+  ADD PRIMARY KEY (`id_commentaire`),
+  ADD KEY `id_utilisateur` (`id_utilisateur`);
+
+ALTER TABLE `commentaires`
+  MODIFY `id_commentaire` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+ALTER TABLE `commentaires`
+  ADD CONSTRAINT `commentaires_ibfk_1` FOREIGN KEY (`id_utilisateur`) REFERENCES `utilisateurs` (`id`);
+COMMIT;
+
+
