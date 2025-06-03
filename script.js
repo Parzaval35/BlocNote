b22c769ccb145f838373cee246351ba51aeae87c
Moijaiimelaglace <lucrenard66@gmail.com>
2025-06-03 09:05:42 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 // JavaScript to toggle night mode background
 document.addEventListener('DOMContentLoaded', function () {
     const nightModeCheckbox = document.getElementById('nightModeCheckbox');
+    const logo = document.getElementById('logo');
+
     nightModeCheckbox.addEventListener('change', function () {
         if (this.checked) {
             document.body.classList.add('night-mode');
+            logo.src = "images/dark_logo.png"; 
         } else {
             document.body.classList.remove('night-mode');
+            logo.src = "images/logo.png";
         }
     });
 });
