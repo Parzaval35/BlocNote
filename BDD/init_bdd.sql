5e295b32208535c04c82af664253b9205cb538e0
Samadi0 <chaymae.samadi16@gmail.com>
2025-06-03 15:53:49 +0200

    Update init_bdd.sql

--- BDD/init_bdd.sql
+++ BDD/init_bdd.sql
@@ -15,7 +15,8 @@ CREATE TABLE `commentaires` (
   `id_commentaire` int(11) NOT NULL,
   `id_utilisateur` int(11) NOT NULL,
   `contenu` text NOT NULL,
-  `id_parent` int(11) DEFAULT NULL
+  `id_parent` int(11) DEFAULT NULL,
+  `id_page` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 ALTER TABLE `commentaires`
