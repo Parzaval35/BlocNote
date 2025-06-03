d4cd03f32d3f7523ddac62afc3096f74bbd142a0
Plagiar57 <demonus055@gmail.com>
2025-06-03 15:13:18 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,36 @@
-// JavaScript to toggle night mode background
 document.addEventListener('DOMContentLoaded', function () {
     const nightModeCheckbox = document.getElementById('nightModeCheckbox');
     const logo = document.getElementById('logo');
 
+    // charge le mode 
+    const savedNightMode = localStorage.getItem('nightMode');
+    if (savedNightMode === 'enabled') {
+        document.body.classList.add('night-mode');
+        nightModeCheckbox.checked = true;
+        logo.src = "images/dark_logo.png";
+    } else {
+        document.body.classList.remove('night-mode');
+        nightModeCheckbox.checked = false;
+        logo.src = "images/logo.png";
+    }
+
     nightModeCheckbox.addEventListener('change', function () {
         if (this.checked) {
             document.body.classList.add('night-mode');
-            logo.src = "images/dark_logo.png"; 
+            logo.src = "images/dark_logo.png";
+            localStorage.setItem('nightMode', 'enabled');
         } else {
             document.body.classList.remove('night-mode');
             logo.src = "images/logo.png";
+            localStorage.setItem('nightMode', 'disabled');
         }
     });
-});
 
-const buttons = document.querySelectorAll(".accordion-btn");
-buttons.forEach((btn) => {
-btn.addEventListener("click", () => {
-        const panel = btn.nextElementSibling;
-        panel.classList.toggle("open");
-  });
+    const buttons = document.querySelectorAll(".accordion-btn");
+    buttons.forEach((btn) => {
+        btn.addEventListener("click", () => {
+            const panel = btn.nextElementSibling;
+            panel.classList.toggle("open");
+        });
+    });
 });
