139aa6bc5216b5844bd592883679b58ea20aa635
Plagiar57 <demonus055@gmail.com>
2025-06-04 15:44:39 +0200

    Add files via upload

---
+++ maintenance.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>Page en maintenance</title>
+    <link rel="stylesheet" href="style.css" />
+</head>
+<body>
+    <div class="fixed-menu">
+        <button class="connexion-btn" onclick="window.location.href='connexion.php'">Connexion</button>
+        <button class="inscription-btn" onclick="window.location.href='inscription.php'">Inscription</button>
+        <label class="switch">
+            <input type="checkbox" id="nightModeCheckbox">
+            <span></span>
+        </label>
+    </div>
+    <header>
+        <div class="logo"><img id="logo" src="images/logo.png" alt="Block-Note Logo" style="height: 80px;"/></div>
+        <nav>
+            <a href="index.php">Home</a>
+            <a href="Maj.php">Dernières MàJ</a>
+            <a href="blocs.php">Block</a>
+            <a href="Creatures.html">Mob</a>
+            <a href="menu_tuto.php">Tutoriels</a>
+        </nav>
+    </header>
+    <main>
+        <div class="maintenance"><img id="logo" src="images/cree_boom.gif" alt="boom" style="height: 2rm;"/></div>
+        <h1>En reconstruction...</h1>
+        <h6>(Un creeper de merde a explosé)</h6>
+        <a href="#" class="top-link">Retour en haut</a>
+    </main>
+    <footer>
+        <a href="#politique-confidentialite">Politique de confidentialité</a> |
+        <a href="#mentions-legales">Mentions légales</a>
+    </footer>
+    <script src="script.js"></script>
+</body>
+</html>
