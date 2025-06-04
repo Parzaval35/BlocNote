e3f7f0cefa24b6a010e9d63944ce4a7a4e7296ba
Samadi0 <chaymae.samadi16@gmail.com>
2025-06-04 14:51:39 +0200

    Update deconnexion.php

--- deconnexion.php
+++ deconnexion.php
@@ -1,6 +1,22 @@
 <?php
 session_start();
+session_unset();
 session_destroy();
-header('Location: connexion.php');
-exit;
 ?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="UTF-8">
+  <title>Déconnexion</title>
+  <link rel="stylesheet" href="style.css">
+  <meta http-equiv="refresh" content="3;url=index.php">
+
+</head>
+<body>
+<div class="success-box">
+  <div class="success-message">
+    <p>Déconnexion réussie ! Vous allez être redirigé vers la page d'accueil.</p>
+</div>
+</div>
+</body>
+</html>
