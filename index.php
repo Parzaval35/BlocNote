ac7781a09e3d0d0b75d13a9866ad55e28f379f2d
Parzaval35 <killian.prat35@gmail.com>
2025-06-11 12:58:23 +0200

    Update index.php

--- index.php
+++ index.php
@@ -71,7 +71,7 @@
             </article>
         </section>
 
-        <a href="#" class="top-link">Retour en haut</a>
+        <a href="" class="top-link">Retour en haut</a>
     </main>
     <footer>
         <a href="#politique-confidentialite">Politique de confidentialité</a> |
